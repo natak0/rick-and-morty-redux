7b40278a718bfff2059b5e552c6aeb92865670f3
natak0 <bcnatako@gmail.com>
2023-02-20 22:33:34 +0100

    build for gh pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="app-container">
         <Navbar />
         <Routes>
