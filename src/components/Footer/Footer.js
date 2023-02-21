aebd10af0f8ee952ea403c6d6df04e6ba96060bc
natak0 <bcnatako@gmail.com>
2023-02-21 10:01:31 +0100

    add link to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,19 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <footer className="footer"></footer>;
+  return (
+    <footer className="footer">
+      <p>
+        Created with{" "}
+        <a
+          className="footer__link"
+          href="https://rickandmortyapi.com/documentation/"
+        >
+          Rick and Morty API
+        </a>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
